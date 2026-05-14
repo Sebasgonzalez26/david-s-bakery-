70c8305f319939cf9d5c7b3796386b5c12dad5dd
Sebasgonzalez26 <147010566+Sebasgonzalez26@users.noreply.github.com>
2026-05-14 12:17:29 -0600

    Cascaron de proyectos API, DA y Flujo
    - Cascarones de PedidoController, PedidoDA, PedidoFlujo
    - Solución actualizada con referencias entre proyectos

---
+++ Bakerys.API/DA/PedidoDA.cs
@@ -0,0 +1,62 @@
+﻿
+
+using Abstracciones.Interfaces.DA;
+using Abstracciones.Modelos;
+using Microsoft.Data.SqlClient;
+
+namespace DA
+{
+    public class PedidoDA : IPedidoDA
+    {
+
+        private IRepositorioDapper _repositorioDapper;
+
+        private SqlConnection _sqlconexion;
+
+        public PedidoDA(IRepositorioDapper repositorioDapper)
+        {
+            _repositorioDapper = repositorioDapper;
+            _sqlconexion = _repositorioDapper.ObtenerRepositorio();
+        }
+
+        public Task<int> ActualizarEstado(int id, int estadoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Agregar(PedidoRequest pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> AgregarDetalle(DetallePedidoRequest detalle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PedidoResponse>> Buscar(string? busqueda, int? estadoId, DateOnly? fechaDesde, DateOnly? fechaHasta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Cancelar(int id, string? motivo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PedidoResponse>> Obtener()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PedidoResponse> Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<EstadoPedidoResponse>> ObtenerEstados()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
