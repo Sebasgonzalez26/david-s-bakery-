7e4131528c01dae0433ef6286655e87ddbcd21a3
Sebasgonzalez26 <147010566+Sebasgonzalez26@users.noreply.github.com>
2026-05-14 12:17:29 -0600

    Cascaron de proyectos API, DA y Flujo
    - Cascarones de PedidoController, PedidoDA, PedidoFlujo
    - Solución actualizada con referencias entre proyectos

---
+++ Bakerys.API/API/Controllers/PedidoController.cs
@@ -0,0 +1,63 @@
+﻿using Abstracciones.Interfaces.API;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : ControllerBase, IPedidoController
+    {
+
+        private IPedidoFlujo _pedidoFlujo;
+        private ILogger<PedidoController> _logger;
+
+        public PedidoController(IPedidoFlujo pedidoFlujo, ILogger<PedidoController> logger)
+        {
+            _pedidoFlujo = pedidoFlujo;
+            _logger = logger;
+        }
+
+        public Task<IActionResult> ActualizarEstado(int id, int estadoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Agregar(PedidoRequest pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> AgregarDetalle(DetallePedidoRequest detalle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Buscar(string? busqueda, int? estadoId, DateOnly? fechaDesde, DateOnly? fechaHasta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Cancelar(int id, string? motivo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Obtener()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> ObtenerEstados()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bakerys.API/Flujo/PedidoFlujo.cs
@@ -0,0 +1,58 @@
+﻿
+
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+
+namespace Flujo
+{
+    public class PedidoFlujo : IPedidoFlujo
+    {
+
+        private IPedidoDA _pedidoDA;
+
+        public PedidoFlujo(IPedidoDA pedidoDA)
+        {
+            _pedidoDA = pedidoDA;
+        }
+
+        public Task<int> ActualizarEstado(int id, int estadoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Agregar(PedidoRequest pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> AgregarDetalle(DetallePedidoRequest detalle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PedidoResponse>> Buscar(string? busqueda, int? estadoId, DateOnly? fechaDesde, DateOnly? fechaHasta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Cancelar(int id, string? motivo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PedidoResponse>> Obtener()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PedidoResponse> Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<EstadoPedidoResponse>> ObtenerEstados()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
