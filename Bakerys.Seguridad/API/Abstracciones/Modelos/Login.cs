0fa34753919254708bac6f3cffc72289112d2125
Sebasgonzalez26 <147010566+Sebasgonzalez26@users.noreply.github.com>
2026-06-07 16:22:20 -0600

    Add RegistroRequest model with CodigoRegistro field

--- Bakerys.Seguridad/API/Abstracciones/Modelos/Login.cs
+++ Bakerys.Seguridad/API/Abstracciones/Modelos/Login.cs
@@ -4,8 +4,7 @@ namespace Abstracciones.Modelos
 {
     public class LoginRequest
     {
-        [Required] public string NombreUsuario     { get; set; } = string.Empty;
-        [Required] public string PasswordHash      { get; set; } = string.Empty;
         [Required][EmailAddress] public string CorreoElectronico { get; set; } = string.Empty;
+        [Required] public string PasswordHash { get; set; } = string.Empty;
     }
 }
