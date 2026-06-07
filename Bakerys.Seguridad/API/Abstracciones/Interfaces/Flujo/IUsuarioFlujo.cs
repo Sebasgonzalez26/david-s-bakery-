f8833d5ea64f10cecf7a42acfbd3a46d607feee3
Sebasgonzalez26 <147010566+Sebasgonzalez26@users.noreply.github.com>
2026-06-07 15:36:43 -0600

    Add IUsuarioFlujo interface

---
+++ Bakerys.Seguridad/API/Abstracciones/Interfaces/Flujo/IUsuarioFlujo.cs
@@ -0,0 +1,9 @@
+using Abstracciones.Modelos;
+
+namespace Abstracciones.Interfaces.Flujo
+{
+    public interface IUsuarioFlujo
+    {
+        Task<Guid> CrearUsuario(UsuarioBase usuario);
+    }
+}
